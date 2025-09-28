9c08e3620668f6b7391cea11a138cae9153d4a2d
unknown <rfahoubou@gmail.com>
2025-09-28 09:07:27 +0200

    commit de verification

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   // Ajout des modules
-  modules: ['@nuxtjs/@pinia/nuxt'],
+  modules: ['@pinia/nuxt'],
   devtools: { enabled: true },
   
   // Configuration pour le fichier .env
